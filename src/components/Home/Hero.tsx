a2bb4ed8b9c39beede79840e5a74ccc22b891f06
kid-us <kidushh29@gmail.com>
2025-02-04 18:19:44 +0300

    On Load animation added.

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,3 +1,4 @@
+import { motion } from "motion/react";
 import { heroVid } from "../../assets";
 import Container from "../Container/Container";
 import { Link } from "react-router-dom";
@@ -9,23 +10,37 @@ const Hero = () => {
       <Container>
         <div className="flex items-center h-screen">
           <div>
-            <h1 className="text-white lg:text-7xl text-4xl w-80 font-bold">
-              {/* Transforming Properties, Enriching Lives. */}
+            <motion.h1
+              initial={{ opacity: 0, scale: 1.1 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ delay: 0.2, duration: 0.5 }}
+              className="text-white lg:text-7xl text-4xl w-80 font-bold"
+            >
               Revitalizing
               <span className="text-btn"> Assets</span>, Empowering
               <span className="text-btn"> Communities.</span>
-            </h1>
-            <p className="text-gray-200 mt-5 poppins-semibold lg:text-md text-sm">
+            </motion.h1>
+            <motion.p
+              initial={{ opacity: 0, scale: 1.1 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ delay: 0.3, duration: 0.5 }}
+              className="text-gray-200 mt-5 poppins-semibold lg:text-md text-sm"
+            >
               Invest passively like an LP, but earn like a GP with our Co-GP
               real estate fund and deals.
-            </p>
+            </motion.p>
 
-            <div className="flex text-white mt-10 gap-x-6">
+            <motion.div
+              initial={{ opacity: 0, scale: 1.1 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ delay: 0.4, duration: 0.5 }}
+              className="flex text-white mt-10 gap-x-6"
+            >
               <Link to="/" className="bi-facebook"></Link>
               <Link to="/" className="bi-instagram"></Link>
               <Link to="/" className="bi-linkedin"></Link>
               <Link to="/" className="bi-twitter-x"></Link>
-            </div>
+            </motion.div>
           </div>
         </div>
       </Container>
