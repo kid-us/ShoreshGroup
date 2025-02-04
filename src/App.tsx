2c2bd250ae1285b2122d384cf741017e9e201c7b
kid-us <kidushh29@gmail.com>
2025-02-04 11:42:16 +0300

    Theme handled.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme) {
+      document.body.classList.add(savedTheme);
+    } else {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        document.body.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+      } else {
+        document.body.classList.add("light");
+        localStorage.setItem("theme", "light");
+      }
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const currentTheme = localStorage.getItem("theme");
+
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+
+    document.body.classList.remove("dark", "light");
+    document.body.classList.add(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
