380d2ca0d46b818854a24c84ea0cff0e3bb32c55
kid-us <kidushh29@gmail.com>
2025-02-05 22:59:17 +0300

    Line straighted

--- src/components/Home/CoreValues.tsx
+++ src/components/Home/CoreValues.tsx
@@ -158,7 +158,7 @@ const CoreValues = () => {
           fromRef={div7Ref}
           toRef={div4Ref}
           curvature={75}
-          endYOffset={10}
+          endYOffset={20}
           reverse
           dotted
           gradientStartColor="#00adee"
