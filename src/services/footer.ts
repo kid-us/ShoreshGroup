1158af630b12171091f28153592eb14ef949a127
kid-us <kidushh29@gmail.com>
2025-02-06 14:43:37 +0300

    path removed.

--- src/services/footer.ts
+++ src/services/footer.ts
@@ -1,8 +1,8 @@
 import { Nav } from "./nav";
 
 export const footer: Nav[] = [
-  { id: 1, name: "About", path: "/about", icon: "" },
-  { id: 2, name: "Become an Investor", path: "/become-investor" },
+  { id: 1, name: "About", path: "/", icon: "" },
+  { id: 2, name: "Become an Investor", path: "/" },
   { id: 3, name: "Our Assets", icon: "bi-building", path: "/" },
   { id: 4, name: "For Sale", icon: "bi-door-open-fill", path: "/" },
   { id: 5, name: "Recently Sold", icon: "bi-buildings-fill", path: "/" },
