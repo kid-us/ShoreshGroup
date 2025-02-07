6392291b220fee17ff8760b6dbf15c1e9ab7f0f4
kid-us <kidushh29@gmail.com>
2025-02-07 11:39:12 +0300

    Footer new background color and some design fixed.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,15 +5,15 @@ import { footer } from "../../services/footer";
 
 const Footer = () => {
   return (
-    <Container>
-      <div className="border-t border-b border-gray-200">
-        <div className="lg:flex justify-between lg:mt-10 lg:mb-5 mt-10 mb-6">
+    <div className="bg-black/90">
+      <Container>
+        <div className="lg:flex justify-between pt-14 lg:pb-0 pb-5">
           <div className="lg:flex items-center">
-            <div className="lg:block grid grid-cols-2 lg:space-x-20 gap-y-3">
+            <div className="lg:block grid grid-cols-2 lg:gap-x-20 lg:space-x-20 gap-x-5 gap-y-3">
               {footer.map((nav) => (
                 <Link
                   to={nav.path}
-                  className="text-gray-700 hover:text-black transition-colors text-sm"
+                  className="lg:mb-0 mb-3 text-gray-300 lg:text-sm hover:text-white hover:text-[15px] transition-colors"
                 >
                   {nav.name}
                 </Link>
@@ -21,51 +21,55 @@ const Footer = () => {
               <div className="lg:flex hidden space-x-20 mt-5 ">
                 <Link
                   to={"/"}
-                  className="text-gray-700 text-sm hover:text-black transition-colors"
+                  className="text-gray-300 lg:text-sm hover:text-white hover:text-[15px] transition-colors"
                 >
                   Privacy Policy
                 </Link>
                 <Link
                   to={"/"}
-                  className="text-gray-700 text-sm hover:text-black transition-colors"
+                  className="text-gray-300 lg:text-sm hover:text-white hover:text-[15px] transition-colors"
                 >
                   Terms of Service
                 </Link>
               </div>
-              <p className="lg:block hidden text-xs text-gray-500 mt-6">
+            </div>
+          </div>
+
+          <div className="flex items-center lg:mt-0 my-8">
+            <div>
+              <div className="flex gap-x-3">
+                <img src={logo} alt="Logo" className="w-16 h-16" />
+                <p className="flex items-center text-2xl font-extrabold logo-font uppercase text-white">
+                  SHORESH GROUP
+                </p>
+              </div>
+              <p className="lg:block hidden text-xs text-btn mt-6">
                 &copy;2025 SHORESH GROUP. All right reserved.
               </p>
             </div>
           </div>
 
-          <div className="flex items-center gap-x-3 lg:mt-0 my-8">
-            <img src={logo} alt="Logo" className="w-16 h-16" />
-            <p className="flex items-center text-2xl font-extrabold logo-font uppercase">
-              SHORESH GROUP
-            </p>
-          </div>
-
           <div className="lg:hidden flex gap-x-10">
             <Link
               to={"/"}
-              className="text-gray-700 text-sm hover:text-black transition-colors"
+              className="text-gray-300 lg:text-sm hover:text-white hover:text-[15px] transition-colors"
             >
               Privacy Policy
             </Link>
             <Link
               to={"/"}
-              className="text-gray-700 text-sm hover:text-black transition-colors"
+              className="text-gray-300 lg:text-sm hover:text-white hover:text-[15px] transition-colors"
             >
               Terms of Service
             </Link>
           </div>
 
-          <p className="lg:hidden block text-xs text-amber-600 mt-5">
+          <p className="lg:hidden block text-xs text-btn mt-10">
             &copy;2025 SHORESH GROUP. All right reserved.
           </p>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </div>
   );
 };
 
