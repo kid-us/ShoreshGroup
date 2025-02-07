9549b324c5007c21fcf2499e581bec455f2e07b4
kid-us <kidushh29@gmail.com>
2025-02-07 11:00:47 +0300

    Meaningful text added.

--- src/components/Home/Mission.tsx
+++ src/components/Home/Mission.tsx
@@ -14,15 +14,15 @@ const Mission = () => {
   const mission: Mission[] = [
     {
       id: 1,
-      name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, suscipit, voluptas placeat magni unde at deserunt possimus a quas vero laborum enim optio assumenda aspernatur dolore voluptatibus et illo facilis.",
+      name: "Empower Investors:Provide transparent opportunities and deliver exceptional returns to our investors. We build lasting trust by offering clear insights into our strategies and performance, ensuring every investor feels confident and valued.",
     },
     {
       id: 2,
-      name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, suscipit, voluptas placeat magni unde at deserunt possimus a quas vero laborum enim optio assumenda aspernatur dolore voluptatibus et illo facilis.",
+      name: "Innovate Sustainably: Implement cutting-edge, responsible strategies for lasting growth. By integrating advanced technologies with sustainable practices, we ensure our growth not only benefits our stakeholders but also contributes positively to the environment and society.",
     },
     {
       id: 3,
-      name: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, suscipit, voluptas placeat magni unde at deserunt possimus a quas vero laborum enim optio assumenda aspernatur dolore voluptatibus et illo facilis.",
+      name: "Build Communities: Create vibrant neighborhoods through thoughtful, community-focused development. Our developments are designed to enhance local quality of life, fostering safe and thriving",
     },
   ];
 
@@ -48,15 +48,15 @@ const Mission = () => {
                 <h1 className="lg:text-3xl text-2xl poppins-bold text-white">
                   Mission
                 </h1>
-                <p className="mt-5 lg:text-lg">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Neque, suscipit, voluptas placeat magni unde at deserunt
-                  possimus a quas vero laborum enim optio assumenda aspernatur
-                  dolore voluptatibus et illo facilis.
-                </p>
-
-                <p className="mt-4 italic">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing.
+                <p className="mt-5 lg:text-lg text-white">
+                  At Shoresh Group, our mission is to empower our investors,
+                  partners, and communities by delivering innovative and
+                  transparent real estate solutions. We strive to create
+                  sustainable value through strategic property acquisitions,
+                  active asset management, and a commitment to excellence. By
+                  leveraging industry expertise and a forward-thinking approach,
+                  we aim to build vibrant communities, generate competitive
+                  returns, and secure the financial futures of all stakeholders.
                 </p>
               </div>
             </div>
@@ -82,9 +82,9 @@ const Mission = () => {
                     onClick={() => setMissionId(m.id)}
                     className={`${
                       m.id === missionId
-                        ? "lg:col-span-3 col-span-4 border border-amber-300 lg:shadow-[8px_10px_3px_0px_black] shadow-[4px_6px_3px_0px_black] bg-white rounded"
+                        ? "lg:col-span-3 col-span-4"
                         : "col-span-4 border border-black/20 rounded"
-                    }  flex justify-center items-center p-8`}
+                    }  flex justify-center items-center p-5 font-medium border border-amber-300 lg:shadow-[8px_10px_3px_0px_black] shadow-[4px_6px_3px_0px_black] bg-white rounded`}
                   >
                     <p className={``}>{m.name}</p>
                   </motion.div>
