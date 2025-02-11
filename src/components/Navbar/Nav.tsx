6c56a45843182f890e1cebc3cb6c438d3f68dcb2
Kidus <98009847+kid-us@users.noreply.github.com>
2025-02-11 08:49:20 +0300

    Update Nav.tsx

--- src/components/Navbar/Nav.tsx
+++ src/components/Navbar/Nav.tsx
@@ -58,7 +58,7 @@ const Nav = ({ bg, onMenuOpen }: Props) => {
         >
           <img src={logo} alt="Logo" className="w-13" />
 
-          <span className="items-center flex logo-font logo-font uppercase underline underline-offset-2 decoration-amber-400">
+          <span className="items-center flex logo-font logo-font uppercase underline underline-offset-2 decoration-amber-400 decoration-1">
             Shoresh Group
           </span>
         </motion.a>
