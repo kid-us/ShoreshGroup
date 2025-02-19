d1a371ffb941a26cbd192a85f4bbbf54289b5a8d
kid-us <kidushh29@gmail.com>
2025-02-19 17:52:06 +0300

    New advantage animation on hover.

--- src/components/Home/Advantage.tsx
+++ src/components/Home/Advantage.tsx
@@ -1,5 +1,6 @@
 import { motion } from "motion/react";
 import Container from "../Container/Container";
+import { useState } from "react";
 
 interface Advantage {
   id: number;
@@ -8,34 +9,36 @@ interface Advantage {
 }
 
 const Advantage = () => {
+  const [hoveredItem, setHoveredItem] = useState<number>(0);
+
   const advantages: Advantage[] = [
     {
       id: 1,
-      title: "Family Heritage:",
+      title: "Family Heritage",
       desc: "With a strong entrepreneurial mindset and a commitment to long-term alignment, we focus on creating lasting value. Our family-driven approach ensures flexibility and an unwavering dedication to maximizing opportunities for our partners.",
     },
     {
       id: 2,
-      title: "Expertise:",
+      title: "Expertise",
       desc: " Our experienced investment team brings specialized knowledge across multifamily, commercial, and residential real estate. Supported by robust market research and a strategic investment infrastructure, we consistently identify high-potential opportunities.",
     },
     {
       id: 3,
-      title: "Global Network :",
+      title: "Global Network",
       desc: "Leveraging an extensive network of entrepreneurs, industry executives, and capital market leaders, we access unique opportunities and strengthen our partners’ investment positions.",
     },
     {
       id: 4,
-      title: "Partnership:",
+      title: "Partnership",
       desc: "At Shoresh Group, reputation and integrity are paramount. We focus on building lasting relationships with founders, family-owned businesses, management teams, and co-investors, ensuring long-term success for all involved.",
     },
   ];
   return (
     <Container>
-      <div>
-        <h1 className="font-semibold lg:text-3xl text-2xl lg:mt-16 my-6 lg:mb-10">
+      <div className="lg:my-40 my-20">
+        <p className="text-gray-700 lg:my-16 mb-5 text-center">
           Shoresh Group Partners Advantage
-        </h1>
+        </p>
 
         <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-5">
           {advantages.map((a, index) => (
@@ -47,11 +50,30 @@ const Advantage = () => {
                 x: 0,
                 transition: { delay: index * 0.01, duration: 0.5 },
               }}
-              className="border rounded-xl border-btn shadow p-5"
+              className="relative p-5 cursor-pointer hover:text-gray-500"
+              onMouseEnter={() => setHoveredItem(a.id)}
+              onMouseLeave={() => setHoveredItem(0)}
             >
-              <p className="font-medium text-xl">0{a.id}</p>
-              <p className="font-medium text-xl">{a.title}</p>
-              <p className="text-gray-700 mt-5 text-sm">{a.desc}</p>
+              <div
+                className={`${
+                  hoveredItem === a.id &&
+                  "-translate-y-10 transition-all duration-300"
+                }`}
+              >
+                <p className="font-medium">0{a.id}</p>
+                <p className="font-medium text-4xl">{a.title}</p>
+              </div>
+
+              {hoveredItem === a.id && (
+                <motion.p
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 0.1, duration: 0.5 }}
+                  className="absolute top-14 lg:w-96 bg z-20 p-7 rounded-lg border text-white text-sm"
+                >
+                  {a.desc}
+                </motion.p>
+              )}
             </motion.div>
           ))}
         </div>
