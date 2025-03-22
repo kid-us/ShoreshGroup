1db7afb4d50f3c9ff45931be0a483069f45dabcd
kid-us <kidushh29@gmail.com>
2025-03-23 00:10:41 +0300

    Sold Description info reduced.

--- src/components/Modal/SoldDescription.tsx
+++ src/components/Modal/SoldDescription.tsx
@@ -16,15 +16,15 @@ const SoldDescription = ({ asset }: Props) => {
       <p className="mt-3 text-sm text-gray-700">
         Property Address : <span className="font-bold">{asset?.location}</span>
       </p>
-      <p className="mt-3 text-sm">Purchased Date : {asset?.purchased}</p>
-      <p className="mt-3 text-sm">Sale Date : {asset?.sale}</p>
+      {/* <p className="mt-3 text-sm">Purchased Date : {asset?.purchased}</p> */}
+      {/* <p className="mt-3 text-sm">Sale Date : {asset?.sale}</p> */}
       <p className="mt-3 text-sm">Holding Period : {asset?.holding}</p>
       <p className="mt-3 text-sm">Purchased Price : {asset?.purchasePrice}</p>
       <p className="mt-3 text-sm">Sale Price : {asset?.salePrice}</p>
-      <p className="mt-3 text-sm">Project Cost : {asset?.cost}</p>
-      <p className="mt-3 text-sm">Profit : {asset?.profit}</p>
-      <p className="mt-3 text-sm">ROI : {asset?.roi}</p>
-      <p className="mt-3 text-sm">COC Return : {asset?.coc}</p>
+      {/* <p className="mt-3 text-sm">Project Cost : {asset?.cost}</p> */}
+      {/* <p className="mt-3 text-sm">Profit : {asset?.profit}</p> */}
+      {/* <p className="mt-3 text-sm">ROI : {asset?.roi}</p> */}
+      {/* <p className="mt-3 text-sm">COC Return : {asset?.coc}</p> */}
     </>
   );
 };
